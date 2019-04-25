9e082e29964ad5dc64eeb44ef53c6b9830bec42f
TiffaniD3 <tkd93@me.com>
2019-04-24 20:59:18 -0500

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+ 
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+.connect(db)
+.then(() => console.log('MongoDB Connected'))
+.catch()
