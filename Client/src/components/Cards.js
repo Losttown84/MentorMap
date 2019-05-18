1277c7eb1dd50ccd8ac60e07c7977d24695b5cf1
Yagmur Guzeldereli <ms.guzeldereli@gmail.com>
2019-05-18 10:01:44 -0500

    ok

--- Client/src/components/Cards.js
+++ Client/src/components/Cards.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card'
+import student1 from "./Images/student1.jpg"
 
 
 class Cards extends React.Component {
@@ -11,7 +12,7 @@ class Cards extends React.Component {
    <div className="container">
    <div className="row">
       <Card className="Card3" style={{ width: '30rem' }}>
-            <Image src="holder.js/171x180" rounded />
+            <Image  className="photo1" src={student1} rounded />
          <Card.Body>
             <Card.Title>Full Name</Card.Title>
             <Card.Text>
@@ -21,6 +22,27 @@ class Cards extends React.Component {
             <button id="button5" type="submit" className="btn btn-light">Schedule a Session</button>
          </Card.Body>
        </Card>
+       <Card className="Card3" style={{ width: '30rem' }}>
+       <Image  className="photo1" src={student1} rounded />
+            <Card.Body>
+               <Card.Title>Full Name</Card.Title>
+               <Card.Text>
+               About 
+               </Card.Text>
+               <button id="button5" type="submit" className="btn btn-light">Profile</button>
+               <button id="button5" type="submit" className="btn btn-light">Schedule a Session</button>
+            </Card.Body>
+         </Card><Card className="Card3" style={{ width: '30rem' }}>
+         <Image  className="photo1" src={student1} rounded />
+            <Card.Body>
+               <Card.Title>Full Name</Card.Title>
+               <Card.Text>
+               About 
+               </Card.Text>
+               <button id="button5" type="submit" className="btn btn-light">Profile</button>
+               <button id="button5" type="submit" className="btn btn-light">Schedule a Session</button>
+            </Card.Body>
+         </Card>
        </div>
        </div>
  </div>
