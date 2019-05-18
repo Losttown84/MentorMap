c24b09f19e74d2c5273b96cacc36bc5932ebf354
Yagmur Guzeldereli <ms.guzeldereli@gmail.com>
2019-05-18 00:15:12 -0500

    lets get it

--- Client/src/pages/profile.js
+++ Client/src/pages/profile.js
@@ -6,6 +6,30 @@ render() {
     return (
 <div>
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a id="NavTitle" class="navbar-brand" href="/"><i className="fas fa-chalkboard-teacher" />  MentorMap</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/">Profile</a>
+        </li>
+        <li class="nav-item">
+                <a class="nav-link" href="/" id="tooltip"><i class="fas fa-question"><span class="tooltiptext">Contact Us</span></i></a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button id="button5" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+    </nav>
     <div className="container">
     <div className="row">
     <div id="profilebox" className="jumbotron jumbotron-fluid">
